8fe36068c2a740e020181732ba7a6b36fe118b30
sagateljan akop <sagatelyan.aa@phystech.edu>
2022-10-01 02:06:47 +0300

    example asm

--- seminar-04/examples/example_1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int f(int a, int b, int c, int d);
-
-int main() {
-	int y = sum(1, 2, 1, 1);
-	printf("%d\n", y);
-	return 0;
-}
--- seminar-04/examples/hello_arm.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("Hello from ARM architecture!\n");
-	return 0;
-}
