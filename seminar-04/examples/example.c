c0cf05397741123b816b7ab5a360b75e3989f3d4
sagateljan akop <sagatelyan.aa@phystech.edu>
2022-10-01 02:04:35 +0300

    example

---
+++ seminar-04/examples/example.c
@@ -0,0 +1,16 @@
+
+int sum(int x, int y) {
+	return x + y;
+}
+
+int sum3(int a, int b, int c) {
+	return a + b + c;
+}
+
+int mul(int x, int y) {
+	return x * y;
+}
+
+int mul_add(int a, int b, int c) {
+	return a * b + c;
+}
