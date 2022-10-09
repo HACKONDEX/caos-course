1c23115254a5f8eba36aa1b0931c4e577e84e5fe
sagateljan akop <sagatelyan.aa@phystech.edu>
2022-10-09 14:51:04 +0300

    dynamic memory usage example C

---
+++ seminar-05/examples/dynamic_memory.c
@@ -0,0 +1,27 @@
+#include <inttypes.h>
+#include <malloc.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+    char* str = calloc(50, sizeof(char));
+    scanf("%s", str);
+
+    uint32_t len = 0;
+    for (uint32_t i = 0; str[i] != '\0' ; ++i) {
+        ++len;
+    }
+
+    char* append_str = "Appended";
+    uint32_t append_len = strlen(append_str);
+    for (uint32_t i = len; i < len + append_len; ++i) {
+        str[i] = append_str[i - len];
+    }
+
+    printf("%s", str);
+    
+    free(str);
+    return 0;
+}
