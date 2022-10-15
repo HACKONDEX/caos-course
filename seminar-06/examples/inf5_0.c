41282b8197906877dda45a7318383f1a337e6c06
sagateljan akop <sagatelyan.aa@phystech.edu>
2022-10-15 16:23:06 +0300

    task inf05-0

---
+++ seminar-06/examples/inf5_0.c
@@ -0,0 +1,14 @@
+#include <inttypes.h>
+#include <stdio.h>
+
+extern int *A;
+
+void very_important_function(size_t N) {
+    for (size_t i = 0; i < N; ++i) {
+        int32_t num = 0;
+        scanf("%d", &num);
+        int64_t result = num;
+        result *= A[i];
+        printf("%d\n", result);
+    }    
+}
