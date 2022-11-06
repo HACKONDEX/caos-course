c7be0d7afc6a0991bb3f3d2298c41256f51697b0
sagateljan akop <sagatelyan.aa@phystech.edu>
2022-11-06 18:45:06 +0300

    hello world nostdlib

--- seminar-08-syscalls/nostdlib_examples/example_hello_world.c
+++ seminar-08-syscalls/nostdlib_examples/example_hello_world.c
@@ -2,6 +2,6 @@
 
 void _start() {
     char hellow[] = "Hellow!\n";
-    syscall(__NR_write, 1, hellow, sizeof(hellow));
+    syscall(__NR_write, 1, hellow, sizeof(hellow) - 1); // -1 for '\0'
     syscall(__NR_exit_group, 0);
 }
